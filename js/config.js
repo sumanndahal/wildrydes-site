38154b402c0dfa5f4368e86e0ad45e7d62a551af
Suman Dahal <141977925+sumanndahal@users.noreply.github.com>
2025-05-14 02:43:41 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_KH0SFWOS7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1p95f3pm0rneuag1q3j8rg7jar', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        SECRET_HASH: '1scuaipu29m9erfp412eeb6n4efrc3e6t1om8m8f4l1nal2sk7cg',
+        userPoolClientId: '2g4vebar6budjtuuvh4po76pvs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
